546826039ec0a1434e192ab8da6f74892eb6c19f
cnaranjo <crispool@hotmail.com>
2019-07-03 21:57:19 -0500

    Initial commit

---
+++ src/main/java/ec/com/altiora/AltioraAppApplication.java
@@ -0,0 +1,13 @@
+package ec.com.altiora;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AltioraAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AltioraAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ec/com/altiora/AltioraAppApplicationTests.java
@@ -0,0 +1,16 @@
+package ec.com.altiora;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class AltioraAppApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
